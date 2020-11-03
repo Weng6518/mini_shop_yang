2e1a37208102da79c3de78f5ae4837f0781f5717
Yang <107b33580@maiulst.cjcu.edu.tw>
2020-11-03 16:20:47 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+define('_SHOP_NAME', '洋哥水果店');
+define('_DB_HOST', 'localhost');
+define('_DB_ID', 'root');
+define('_DB_PW', '12345');
+define('_DB_NAME', 'mini_shop');
+define('_UPLOAD_PATH', 'C:/UniServerZ/www/mini_shop/uploads/');
+define('_UPLOAD_URL', 'http://localhost:7777/mini_shop/uploads/');
+
+$admin_array = ['107b33580'];
+// error_reporting(0);
